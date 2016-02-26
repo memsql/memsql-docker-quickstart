18271e64604dcb8441e2de1e7403112562663d1c
Carl Sverre <carl@memsql.com>
2016-02-25 17:32:23 -0800

    release memsql quickstart 5.0.0 (beta)

--- setup_ops.sh
+++ setup_ops.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-VERSION_URL="http://versions.memsql.com/memsql-ops/4.1.11"
+VERSION_URL="http://versions.memsql.com/memsql-ops/5.0.0"
 MEMSQL_VOLUME_PATH="/memsql"
 OPS_URL=$(curl -s "$VERSION_URL" | jq -r .tar)
 
